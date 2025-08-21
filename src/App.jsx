8d2b990047a4c88793226073a47dad86dd1e2434
Michael McCormack <mcctwin@gmail.com>
2025-08-21 16:01:54 -0500

    un-commented old CSS, added new files for future Game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
-import Games from './pages/Games';
+import GamesContainer from './pages/GamesContainer';
 import './App.css'
-import Navbar from './components/Navbar'
 import {BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/games" element={<Games />} />
+        <Route path="/games" element={<GamesContainer />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
