0f3243062feae3f7822d07a55bc68dd885dcde95
Michael McCormack <mcctwin@gmail.com>
2025-08-21 15:01:57 -0500

    Added Home message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,10 @@ function Home() {
       <Navbar />
       <div>
         <h1>Home</h1>
+        <h2>Welcome to GamesMart!</h2>
+        <h3>Your one-stop shop for all things gaming!</h3>
+        <p>Check out the "Games" tab to shop for games.</p>
+        <p>Are you an Admin? check out the "Admin" tab to add game listings</p>
       </div>
     </>
   );
